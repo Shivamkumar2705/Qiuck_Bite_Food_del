39ae8d73f228fb646641b546d044ee625cfde3b6
Shivam Kumar <145675379+Shivamkumar2705@users.noreply.github.com>
2025-08-09 23:08:18 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://qiuck-bite-backend.onrender.com'
+export const url = import.meta.env.VITE_URL;
 export const currency = '₹'
 
 export const assets ={
